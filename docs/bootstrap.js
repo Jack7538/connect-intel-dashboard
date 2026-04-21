31be96fc7df91618c3c683a941b46ca08c722475
Jack7538 <jack7538@users.noreply.github.com>
2026-04-21 17:14:05 +0900

    CONNECT dashboard release 2026-04-21T17:14:05+09:00

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -36,8 +36,8 @@
     }catch(e){ return ''; }
   }
 
-  setStatus('Data: boot 20260421T1050540900 (loading app.js...)');
-  var appUrl = './app.js?v=20260421T1050540900';
+  setStatus('Data: boot 20260421T1704340900 (loading app.js...)');
+  var appUrl = './app.js?v=20260421T1704340900';
   fetch(appUrl, { cache: 'no-store' }).then(function(r){
     if(!r.ok) throw new Error('HTTP ' + r.status + ' for ' + appUrl);
     return r.text();
