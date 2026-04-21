bca5000ded3abd650902399342cfd50c52e50731
Jack7538 <jack7538@users.noreply.github.com>
2026-04-21 13:17:52 +0900

    CONNECT dashboard dev sync 2026-04-21T13:17:52+09:00

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -36,8 +36,8 @@
     }catch(e){ return ''; }
   }
 
-  setStatus('Data: boot 20260421T1316370900 (loading app.js...)');
-  var appUrl = './app.js?v=20260421T1316370900';
+  setStatus('Data: boot 20260421T1317510900 (loading app.js...)');
+  var appUrl = './app.js?v=20260421T1317510900';
   fetch(appUrl, { cache: 'no-store' }).then(function(r){
     if(!r.ok) throw new Error('HTTP ' + r.status + ' for ' + appUrl);
     return r.text();
